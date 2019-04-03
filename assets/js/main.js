dc778f20f5337ee9e6a6f53ecb89b2ca462c271a
Matteo Carpi <matteocarpi94@gmail.com>
2019-04-03 15:08:56 +0200

    a lot of styling

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,10 +12,8 @@ function quit(){
   var burger = document.getElementById('burger');
   var links = document.getElementById('links');
   var quit = document.getElementById('quit');
+  var autoQuit = document.getElementById('menu-item')
   burger.style.padding = '16px 16px 32px 32px';
   links.style.display = 'none';
   quit.style.display = 'none';
 }
-// Facebook Embed
-<div id="fb-root"></div>
-<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v3.2"></script>
